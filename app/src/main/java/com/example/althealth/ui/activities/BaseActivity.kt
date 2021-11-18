6ce1f7d11da41127d6a1a500175f122b69d80173
Khanyisa <kekekhanyisa@gmail.com>
2021-11-18 14:40:06 +0200

    Validating Registry Entries

--- app/src/main/java/com/example/althealth/ui/activities/BaseActivity.kt
+++ app/src/main/java/com/example/althealth/ui/activities/BaseActivity.kt
@@ -7,15 +7,39 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.althealth.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+/**
+* A base activity class is used to define the functions and members which we will use in all the activities.
+* It inherits the AppCompatActivity class so in other activity class we will replace the AppCompatActivity with BaseActivity.
+*/
 
 open class BaseActivity: AppCompatActivity() {
 
     // A global variable for double back press feature.
     private var doubleBackToExitPressedOnce = false
+
+    /**
+     * A function to show the success and error messages in snack bar component.
+     */
+
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage){
+            snackBarView.setBackgroundColor(ContextCompat.getColor(this@BaseActivity, R.color.colorSnackBarError))
+        } else {
+            snackBarView.setBackgroundColor(ContextCompat.getColor(this@BaseActivity, R.color.colorSnackBarSuccess))
+        }
+        snackBar.show()
+    }
 
     // This function is used to hide the status bar and make the screen a full screen activity.
 
--- app/src/main/java/com/example/althealth/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/althealth/ui/activities/RegisterActivity.kt
@@ -3,8 +3,14 @@ package com.example.althealth.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.example.althealth.R
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,16 +20,98 @@ class RegisterActivity : BaseActivity() {
         // Hide the status bar and make the screen a full screen activity.
         hideStatusBar()
 
+        setupActionBar()
+
         val tv_login: TextView = findViewById(R.id.tv_login)
+        val btn_register: Button = findViewById(R.id.btn_register)
 
         tv_login.setOnClickListener{
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
 
+        btn_register.setOnClickListener {
+            validateRegisterDetails()
+        }
+
     }
 
     private fun setupActionBar(){
+
+        val toolbar_register_activity: Toolbar = findViewById(R.id.toolbar_register_activity)
+
+        setSupportActionBar(toolbar_register_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+        }
+
+        toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
+
+    }
+/**
+     A function to validate the entries of a new user.
+    */
+
+    private fun validateRegisterDetails(): Boolean {
+
+        val et_first_name: EditText = findViewById(R.id.et_first_name)
+        val et_last_name: EditText = findViewById(R.id.et_last_name)
+        val et_email: EditText = findViewById(R.id.et_email)
+        val et_password: EditText = findViewById(R.id.et_password)
+        val et_confirm_password: EditText = findViewById(R.id.et_confirm_password)
+        val cb_terms_and_condition: CheckBox = findViewById(R.id.cb_terms_and_condition)
+
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+                }
+
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.registry_successfull), false)
+                true
+            }
+
+        }
+
+
 
     }
 
