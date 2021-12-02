641d093121c6b1fea708f0f4dbf0497229ef0b46
Khanyisa <kekekhanyisa@gmail.com>
2021-12-02 20:45:19 +0200

    Setting up Firestore

---
+++ app/src/main/java/com/example/althealth/firestore/FirestoreClass.kt
@@ -0,0 +1,9 @@
+package com.example.althealth.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+
+}
